15ed73fbb0dc04a46144336fcff64ac4e266e725
Dipayan <ddipayan00@gmail.com>
2021-11-29 20:39:04 +0530

    Last final

--- app.js
+++ app.js
@@ -2,13 +2,30 @@ const express = require('express')
 const path = require('path')
 const app = express()
 let {people} = require('./data')
-
+// Static assets
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({extended: false}))
+// GET METHODS
 app.get('/',(req,res) => {
-    res.status(200).send("HEHE")
+    res.status(200)
 })
 app.get('/api/people',(req,res) => {
     res.status(200).json({success:true,data:people})
 })
+app.post('/login',(req,res) => {
+    const {name} = req.body
+    if(name){
+        return res.send("Hi, "+ name)
+    }
+    return res.status(401).send("Please provide name")
+})
+// POST METHODS
+
+
+
+
+
+
 
 app.listen(5000,()=>{
     console.log("Server started at 5000 port ");
